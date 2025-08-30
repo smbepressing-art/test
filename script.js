6df9de32172d658e248c8b55442ef88e88bfe1c0
smbepressing-art <smbe.pressing@gmail.com>
2025-08-30 02:12:54 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Simple smooth scroll for navigation
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
